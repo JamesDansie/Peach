23f0090cb26a03190336d4ea84dd1dbb7e10a2e5
JamesDansie <jdansie8336@gmail.com>
2019-12-17 21:17:31 -0800

    first attempt at db setup

---
+++ polls/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Dish(models.Model):
+    title = models.CharField(max_length=200)
+    # This is for storing the url to the image
+    img = models.CharField(max_length=200)
+    # if ImageField; https://www.geeksforgeeks.org/imagefield-django-models/
+    # img_file = models.ImageField
+    restaurant_name = models.CharField(max_length=200)
+    # decimal fields can be found here; https://www.geeksforgeeks.org/decimalfield-django-models/
+    price = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
+    description = models.CharField(max_length=200)
+    other_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title

4bcca892058b7e9538a96f02caff0b7193d12d56
JamesDansie <jdansie8336@gmail.com>
2019-12-18 17:04:34 -0800

    added initial dishes. index.html is a proper html now. css is hooked up

--- polls/urls.py
+++ polls/urls.py
@@ -4,12 +4,13 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
+    # generic views (classes) us pk, functions use id's
     # ex: /
     path('', views.IndexView.as_view(), name='index'),
     # ex: /5/
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /5/image/
-    path('<int:pk>/image/', views.image, name='image'),
+    path('<int:dish_id>/image/', views.image, name='image'),
     # ex: /5/vote/
     path('<int:dish_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -10,7 +10,60 @@ class IndexView(generic.ListView):
     context_object_name = 'dish_list'
 
     def get_queryset(self):
-        return Dish.objects.order_by('id')
+        dish_list = Dish.objects.order_by('id')
+        print(dish_list, type(dish_list), len(dish_list))
+        # There's no existing dishes, make some dishes.
+        if(len(dish_list) < 1):
+            d1 = Dish(
+                title = "Red Curry",
+                img = "https://via.placeholder.com/150",
+                restaurant_name = "Thai Place",
+                price = 8.99,
+                description = "Yummy curry!",
+                other_text = "more text",
+                votes = 0
+            )
+            d1.save()
+            
+            d2 = Dish(
+                title = "Fish and Chips",
+                img = "https://via.placeholder.com/150",
+                restaurant_name = "The Fisherman's Wharf",
+                price = 10.99,
+                description = "Pippin' hot fish and chips!",
+                other_text = "Yummy fish and chips!",
+                votes = 1
+            )
+            d2.save()
+
+            d3 = Dish(
+                title = "Burger and Beer",
+                img = "https://via.placeholder.com/150",
+                restaurant_name = "Jimmy's Imaginary Burger Shack",
+                price = 15.99,
+                description = "It is the. best. burger.     ever.",
+                other_text = "might not be the best burger ever",
+                votes = 4
+            )
+            d3.save()
+
+            # laziness wins
+            for x in range(4,9):
+                d = Dish(
+                    title = "Place holder dish " + str(x),
+                    img = "https://via.placeholder.com/150",
+                    restaurant_name = "Restaurant " + str(x),
+                    price = 10.99 + x,
+                    description = "Description " + str(x),
+                    other_text = "Other text " + str(x),
+                    votes = 0
+                )
+                print(d)
+                d.save()
+
+            dish_list = Dish.objects.order_by('id')
+        
+        return dish_list
 
 # def index(request):
 #     dish_list = Dish.objects.order_by('id')
@@ -23,7 +76,7 @@ class IndexView(generic.ListView):
 #     # template = loader.get_template('polls/index.html')
 #     # return HttpResponse(template.render(context, request))
 
-class DetailView(generic.DeleteView):
+class DetailView(generic.DetailView):
     model = Dish
     template_name = 'polls/detail.html'
 
